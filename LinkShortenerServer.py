48a0da1078f593bbf0ae47db3debf7c7231515a4
Ling Vu <vumanhling@icloud.com>
2019-05-04 13:36:20 +0200

    Conform to default port

--- LinkShortenerServer.py
+++ LinkShortenerServer.py
@@ -3,6 +3,7 @@
 import http.server
 import string
 import random
+import os
 
 import requests
 from urllib.parse import unquote, parse_qs
@@ -102,6 +103,7 @@ class Shortener(http.server.BaseHTTPRequestHandler):
 
 
 if __name__ == '__main__':
+    serverport = int(os.environ.get('PORT', 8000))
     server_address = ('', serverport)
     httpd = http.server.HTTPServer(server_address, Shortener)
     httpd.serve_forever()
